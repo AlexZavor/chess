a08ed6d81c22f981739ab0560851c0d6f1d16c89
Alexzavor <enderdefender9@gmail.com>
2024-02-29 20:35:56 -0700

    Auth DAO

--- server/src/main/java/dataAccess/SQLAuthDAO.java
+++ server/src/main/java/dataAccess/SQLAuthDAO.java
@@ -1,29 +1,96 @@
 package dataAccess;
 
 import java.sql.*;
+import java.util.UUID;
+
 import model.AuthData;
 
 public class SQLAuthDAO implements AuthDAO{
     public SQLAuthDAO() {
+        try {
+            DatabaseManager.createDatabase();
+            try (var conn = DatabaseManager.getConnection()) {
+                String[] createStatements = {
+                        """
+                        CREATE TABLE IF NOT EXISTS  auths (
+                          `username` varchar(128) NOT NULL,
+                          `authToken` varchar(256) NOT NULL,
+                          PRIMARY KEY (`username`)
+                        )
+                        """
+                };
+                for (var statement : createStatements) {
+                    try (var preparedStatement = conn.prepareStatement(statement)) {
+                        preparedStatement.executeUpdate();
+                    }
+                }
+            } catch (SQLException ex) {
+                throw new DataAccessException("Unable to configure Auth Database");
+            }
+        } catch (DataAccessException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
     public void clear() {
-
+        var statement = "TRUNCATE auths";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            System.out.println("Failed to clear Auths");
+        }
     }
 
     @Override
     public AuthData createAuth(String username) {
-        return null;
+        AuthData auth = new AuthData(UUID.randomUUID().toString(), username);
+        var statement = "INSERT INTO auths (username, authToken) VALUES (?, ?)";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setString(1, auth.username());
+                ps.setString(2, auth.authToken());
+                if(ps.executeUpdate() != 1) {
+                    throw new DataAccessException("Can't Create auth");
+                }
+
+            }
+        } catch (SQLException | DataAccessException e) {
+            System.out.println(e.getMessage());
+        }
+        return auth;
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        var statement = "SELECT * FROM auths WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)){
+                ps.setString(1,authToken);
+                ResultSet rs = ps.executeQuery();
+                rs.next();
+                var username = rs.getString("username");
+                return new AuthData(authToken, username);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Can't find auth");
+        }
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
-
+        var statement = "DELETE FROM auths WHERE authToken = ?";
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)){
+                ps.setString(1,authToken);
+                if(ps.executeUpdate() != 1) {
+                    throw new DataAccessException("Did weird delete");
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Can't delete User");
+        }
     }
 }
